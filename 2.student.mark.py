088dca7b17b714665533a3c0a89ec8c2e1ba243e
VuAnh183 <dinhvuanh2708@gmail.com>
2024-01-19 14:26:48 +0700

    Unfinished practical

--- 2.student.mark.py
+++ 2.student.mark.py
@@ -1,21 +1,42 @@
 class Student:
-  
-  def __init__(self):
-    self.name = str(input("Enter student name: "))
-    self.age = int(input("Enter student age: "))
     
+  def numStd():
+    numStd = int(input("Enter the number of students: "))
+    
+  def setAttr(self):
+    for i in Student.numStd():
+      self.__std_name = str(input("Enter student name: "))
+      self.__std_age = int(input("Enter student age: "))
+    
+  def getStdName(self):
+    return self.__std_name
+    
+  def getStdAge(self):
+    return self.__std_age
+  
+  
   def describe(self):
-    print(f"Name: {self.name}")
-    print(f"Age: {self.age}")
+    for i in Student.numStd:
+      print(f"Name: {self.std_name}")
+      print(f"Age: {self.std_age}")
 
 class Course:
+  
   def __init__(self):
-    self.name = str(input("Enter course name: "))
+    self.course_name = str(input("Enter course name: "))
     
   def describe(self):
-    print(f"Course name: {self.name}")
+    print(f"Course name: {self.course_name}")
     
+class StudentMark:
   
+  def __init__(self):
+    for i in Course.course_name:
+      self.mark_name = Course.course_name[i]
+      self.mark = int(input(f"Enter mark for {self.mark_name}: "))
+      
+  def describe(self):
+    print(f"Mark for {self.mark_name}: {self.mark}")
   
 def main():
   student = Student()
