9d16a758514aed3002be40e31827b49479661770
VuAnh183 <dinhvuanh2708@gmail.com>
2024-03-13 00:11:28 +0700

    Practical 2

--- 2.student.mark.py
+++ 2.student.mark.py
@@ -23,7 +23,12 @@ class Student:
     return self.__course
   
   def addCourse(self, course_id, course_name):
-    self.__course[course_id] = {"Name": course_name, "Mark": ""}
+    # if course_name in self.__course.values() == True:
+      self.__course[course_id] = {"Name": course_name, "Mark": ""}
+      course_values = self.__course.values()
+      print(course_values)
+    # else: 
+      # print("Course already exists!")
     
   def addMark(self, course_id, mark):
     self.__course[course_id]["Mark"] = mark
@@ -98,32 +103,53 @@ class Function(Student, Course):
       mark = input()
       self.__std_list[x].addMark(c_id, mark)
       
-      
+  
+  # Display students marks 
   def printMark(self):
     print("----------------------------------------------------------------")
     c_id = int(input("Enter course ID: "))
+    
+    # Get course name from list
     print(f"{self.__course_list[c_id].getCourseName()} marks: ")
+    
+    # Get students name and mark
     for x in self.__std_list:
       print(f"{self.__std_list[x].getStdName()}: {self.__std_list[x].getMark(c_id)}")
       
-      
+  # Display student list
   def displayStudentList(self):     
     print("----------------------------------------------------------------")
     print("Student list:")
+    
+    # Check if there are any student in the list 
+    if len(self.__std_list) == 0:
+      print("There are no student record")
+      print("Please add students before trying this function again!") 
+      
+    # Display students names
     count = 1
     for x in self.__std_list:
       print(f"{count}) {self.__std_list[x].getStdName()}")
       count += 1 
-            
+  
+  # Display course list 
   def displayCourseList(self):
     print("----------------------------------------------------------------")
     print("Course list:")
+    
+    # Check if there are any course in the list 
+    if len(self.__course_list) == 0:
+      print("There are no course record")
+      print("Please add courses before trying this function again!") 
+    
+    # Display courses names
     count = 1
     for x in self.__course_list:
       print(f"{count}) {self.__course_list[x].getCourseName()}")
       count += 1  
       
 
+  # List of functions
   def listFuncion(self):
     print("LIST OF FUNCTIONS: \n")
     print("1. Add students")
@@ -139,37 +165,43 @@ class Function(Student, Course):
     match arg:
       case 1: 
         self.add_student()
+        print("\n\n\n")
         print("---")
         return self.listFuncion()
       case 2:
         self.add_course()
+        print("\n\n\n")
         print("---")
         return self.listFuncion()
       case 3:
         self.displayStudentList()
+        print("\n\n\n")
         print("---")
         return self.listFuncion()
       case 4:
         self.displayCourseList()
+        print("\n\n\n")
         print("---")
         return self.listFuncion()
       case 5:
         self.inputMark()
+        print("\n\n\n")
         print("---")
         return self.listFuncion()
       case 6:
         self.printMark()
+        print("\n\n\n")
         print("---")
         return self.listFuncion()
       case 7:
         print("Exit")
         exit()
       case default:
-        print("Invalid ID")
+        print("Invalid")
         print("--------------------------------")
+        print("\n\n\n")
+        print("---")
         return self.listFuncion()
-
-
         
 func = Function()
 
